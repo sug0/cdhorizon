0da010178cab96cf1f5a05b5be3e5d6aebe2a776
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-05-27 18:35:20 +0100

    initial api considerations

---
+++ libglitch/glitch.h
@@ -0,0 +1,10 @@
+#ifndef LIBGLITCH_H
+#define LIBGLITCH_H
+
+#include <goimg/goimg.h>
+
+extern void horizon_Glitch(Image_t *dst, Image_t *src, horizon_Script *script); /*{*/
+//    horizon_ScriptRun(dst, src, script);
+//}
+
+#endif
