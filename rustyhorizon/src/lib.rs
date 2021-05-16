d3e3bb55618c5882886767710640f342e4772c32
Tiago Carvalho <sugoiuguu@tfwno.gf>
2021-05-16 18:31:13 +0100

    glitch img

--- rustyhorizon/src/lib.rs
+++ rustyhorizon/src/lib.rs
@@ -7,6 +7,8 @@ use std::collections::HashMap;
 
 pub enum HorizonError {
     InvalidScript(String),
+    DecodeErr,
+    EncodeErr,
 }
 
 pub enum Param {
@@ -87,7 +89,7 @@ unsafe fn run_script_unsafe(script: &[u8], image: &[u8], params: HashMap<String,
     }
     let params = ffi::HorizonParams { len: list.len(), list: list.as_ptr() };
 
-    // script reader
+    // compile script
     let mut reader = SliceReader {
         data: script.as_ptr(),
         len: script.len(),
@@ -99,7 +101,6 @@ unsafe fn run_script_unsafe(script: &[u8], image: &[u8], params: HashMap<String,
         func: lua_error_to_str,
     };
 
-    // compile script
     let ok = ffi::horizon_ScriptCompileCtxParams(
         &params as *const _,
         &mut error_ctx as *mut _,
@@ -111,7 +112,56 @@ unsafe fn run_script_unsafe(script: &[u8], image: &[u8], params: HashMap<String,
         return Err(HorizonError::InvalidScript(error_ctx_data));
     }
 
-    unimplemented!()
+    // decode image
+    let mut reader = SliceReader {
+        data: image.as_ptr(),
+        len: image.len(),
+        ptr: 0,
+    };
+
+    let fmt = ffi::im_decode(
+        &mut ctx.src as *mut _,
+        read_slice,
+        &mut reader as *mut _ as *mut _,
+    );
+    if fmt.is_null() {
+        return Err(HorizonError::DecodeErr);
+    }
+
+    // glitch image
+    ffi::im_initimg_nrgba(
+        &mut ctx.dst as *mut _,
+        ctx.src.w,
+        ctx.src.h,
+        ffi::im_std_allocator,
+    );
+    error_ctx_data.clear();
+
+    let ok = ffi::horizon_GlitchCtx(
+        &mut error_ctx as *mut _,
+        &mut ctx.dst as *mut _,
+        &ctx.src as *const _,
+        &mut ctx.script as *mut _,
+    );
+    if ok != 0 {
+        return Err(HorizonError::InvalidScript(error_ctx_data));
+    }
+
+    // encode image
+    let mut dest = Vec::new();
+    let fmt = "PNG\0".as_ptr();
+
+    let ok = ffi::im_encode(
+        &ctx.dst as *const _,
+        fmt,
+        write_vec,
+        &mut dest as *mut _ as *mut _,
+    );
+    if ok < 0 {
+        return Err(HorizonError::EncodeErr);
+    }
+
+    Ok(dest)
 }
 
 extern "C" fn read_slice(slice: *mut c_void, buf: *mut u8, bufsiz: usize) -> i32 {
